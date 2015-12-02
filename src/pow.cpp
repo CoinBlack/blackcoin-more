37ec55836aa6f063f8c603f6935365550b4f35c4
Jorge Timón <jtimon@jtimon.cc>
2015-12-02 03:15:42 +0100

    Consensus: Decouple pow.cpp from util.h

--- src/pow.cpp
+++ src/pow.cpp
@@ -90,11 +90,11 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
 
     // Check range
     if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit))
-        return error("CheckProofOfWork(): nBits below minimum work");
+        return false;
 
     // Check proof of work matches claimed amount
     if (UintToArith256(hash) > bnTarget)
-        return error("CheckProofOfWork(): hash doesn't match nBits");
+        return false;
 
     return true;
 }
