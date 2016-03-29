88c725c6ac9f15d82956aa038d759b6d53cf7e31
Pieter Wuille <pieter.wuille@gmail.com>
2016-03-29 16:46:20 +0200

    Reformat version in UpdateTip and other messages

--- src/main.cpp
+++ src/main.cpp
@@ -3575,6 +3575,14 @@ bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& sta
         return state.Invalid(error("%s: block timestamp too far in the future", __func__),
                              REJECT_INVALID, "time-too-new");
 
+    /*
+    // Reject outdated version blocks when 95% (75% on testnet) of the network has upgraded:
+    for (int32_t version = 2; version < 5; ++version) // check for version 2, 3 and 4 upgrades
+        if (block.nVersion < version && IsSuperMajority(version, pindexPrev, consensusParams.nMajorityRejectBlockOutdated, consensusParams))
+            return state.Invalid(false, REJECT_OBSOLETE, strprintf("bad-version(0x%08x)", version - 1),
+                                 strprintf("rejected nVersion=0x%08x block", version - 1));
+    */
+
     return true;
 }
 
