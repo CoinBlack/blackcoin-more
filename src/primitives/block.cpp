1ab443eec77088e099fc5617464c50d703ce2847
Pieter Wuille <pieter.wuille@gmail.com>
2016-03-29 16:46:20 +0200

    Reformat version in UpdateTip and other messages

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -28,7 +28,7 @@ uint256 CBlockHeader::GetPoWHash() const
 std::string CBlock::ToString() const
 {
     std::stringstream s;
-    s << strprintf("CBlock(hash=%s, ver=%d, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u, vchBlockSig=%s)\n",
+    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u, vchBlockSig=%s)\n",
         GetHash().ToString(),
         nVersion,
         hashPrevBlock.ToString(),
