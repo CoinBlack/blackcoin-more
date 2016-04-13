d50d2bbdedd92a50f766bcfd9e05c66f80d71ead
Cory Fields <cory-nospam-@coryfields.com>
2016-04-12 20:41:39 -0400

    net: resolve outside of storage structures
    Rather than allowing CNetAddr/CService/CSubNet to launch DNS queries, require
    that addresses are already resolved.
    
    This greatly simplifies async resolve logic, and makes it harder to
    accidentally leak DNS queries.

--- src/init.cpp
+++ src/init.cpp
@@ -1171,10 +1171,11 @@ bool AppInit2(Config& config, boost::thread_group& threadGroup, CScheduler& sche
 
     if (mapArgs.count("-externalip")) {
         BOOST_FOREACH(const std::string& strAddr, mapMultiArgs["-externalip"]) {
-            CService addrLocal(strAddr, GetListenPort(), fNameLookup);
-            if (!addrLocal.IsValid())
+            CService addrLocal;
+            if (Lookup(strAddr.c_str(), addrLocal, GetListenPort(), fNameLookup) && addrLocal.IsValid())
+                AddLocal(addrLocal, LOCAL_MANUAL);
+            else
                 return InitError(ResolveErrMsg("externalip", strAddr));
-            AddLocal(CService(strAddr, GetListenPort(), fNameLookup), LOCAL_MANUAL);
         }
     }
 
