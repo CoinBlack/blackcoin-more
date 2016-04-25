622fa5bb1965500e8a60447af3e18016456a9aa7
Pieter Wuille <pieter.wuille@gmail.com>
2016-04-25 12:32:01 +0200

    Treat overly long scriptPubKeys as unspendable

--- src/script/script.h
+++ src/script/script.h
@@ -637,7 +637,7 @@ public:
 
     bool IsUnspendable() const
     {
-           return (size() > 0 && *begin() == OP_RETURN);
+        return (size() > 0 && *begin() == OP_RETURN) || (size() > MAX_SCRIPT_SIZE);
     }
 
     void clear()
