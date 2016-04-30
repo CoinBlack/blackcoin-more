69de80c238dd1fc2a1ef88dd63068278ecc27caf
21E14 <21xe14@gmail.com>
2016-04-29 23:45:20 -0400

    Remove obsolete reference to CValidationState from UpdateCoins.

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -733,7 +733,7 @@ void CTxMemPool::check(const CCoinsViewCache *pcoins) const
             CValidationState state;
             PrecomputedTransactionData txdata(tx);
             assert(CheckInputs(tx, state, mempoolDuplicate, false, 0, false, txdata, NULL));
-            UpdateCoins(tx, state, mempoolDuplicate, 1000000);
+            UpdateCoins(tx, mempoolDuplicate, 1000000);
         }
     }
     unsigned int stepsSinceLastRemove = 0;
@@ -748,7 +748,7 @@ void CTxMemPool::check(const CCoinsViewCache *pcoins) const
         } else {
             PrecomputedTransactionData txdata(entry->GetTx());
             assert(CheckInputs(entry->GetTx(), state, mempoolDuplicate, false, 0, false, txdata, NULL));
-            UpdateCoins(entry->GetTx(), state, mempoolDuplicate, 1000000);
+            UpdateCoins(entry->GetTx(), mempoolDuplicate, 1000000);
             stepsSinceLastRemove = 0;
         }
     }
