f50bfbf7fe19a757b1aee16323ea4ab14dc8e70b
Tyler Hardin <th020394@gmail.com>
2016-05-11 22:28:02 -0400

    Qt: Add option to hide the system tray icon
    My changes leave all tray icon and menu creation/initialization logic
    untouched. It only shows or hides the icon according to the setting.
    
    A new checkbox was added to the OptionsDialog under the Window tab. A
    bool option named "hideTrayIcon" was added to OptionsModel. This
    checkbox was mapped like other all options to the OptionsModel.
    
    A signal was added to the OptionsModel for broadcasting changes the the
    hideTrayIcon option. This signal was connected to a new slot added to
    BitcoinGUI named setTrayIconVisible(bool). The slot simply hides or
    shows the trayIcon in BitcoinGUI according to the parameter recieved.

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -500,6 +500,16 @@ void BitcoinGUI::setClientModel(ClientModel *clientModel)
         }
 #endif // ENABLE_WALLET
         unitDisplayControl->setOptionsModel(clientModel->getOptionsModel());
+        
+        OptionsModel* optionsModel = clientModel->getOptionsModel();
+        if(optionsModel)
+        {
+            // be aware of the tray icon disable state change reported by the OptionsModel object.
+            connect(optionsModel,SIGNAL(hideTrayIconChanged(bool)),this,SLOT(setTrayIconVisible(bool)));
+        
+            // initialize the disable state of the tray icon with the current value in the model.
+            setTrayIconVisible(optionsModel->getHideTrayIcon());
+        }
     } else {
         // Disable possibility to show main window via action
         toggleHideAction->setEnabled(false);
@@ -562,7 +572,7 @@ void BitcoinGUI::createTrayIcon(const NetworkStyle *networkStyle)
     QString toolTip = tr("Bitcoin Core client") + " " + networkStyle->getTitleAddText();
     trayIcon->setToolTip(toolTip);
     trayIcon->setIcon(networkStyle->getTrayAndWindowIcon());
-    trayIcon->show();
+    trayIcon->hide();
 #endif
 
     notificator = new Notificator(QApplication::applicationName(), trayIcon, this);
@@ -1132,6 +1142,14 @@ void BitcoinGUI::showProgress(const QString &title, int nProgress)
     }
     else if (progressDialog)
         progressDialog->setValue(nProgress);
+}
+
+void BitcoinGUI::setTrayIconVisible(bool fHideTrayIcon)
+{
+    if (trayIcon)
+    {
+        trayIcon->setVisible(!fHideTrayIcon);
+    }
 }
 
 static bool ThreadSafeMessageBox(BitcoinGUI *gui, const std::string& message, const std::string& caption, unsigned int style)
