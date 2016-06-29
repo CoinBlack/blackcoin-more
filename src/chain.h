a99d1b2425b56ed2b52379f69df48359deb1f019
MarcoFalke <falke.marco@gmail.com>
2016-06-29 11:48:51 +0200

    [doc] Fix typos in comments, doxygen: Fix comment syntax

--- src/chain.h
+++ src/chain.h
@@ -138,16 +138,16 @@ enum BlockStatus {
     BLOCK_VALID_MASK         =   BLOCK_VALID_HEADER | BLOCK_VALID_TREE | BLOCK_VALID_TRANSACTIONS |
                                  BLOCK_VALID_CHAIN | BLOCK_VALID_SCRIPTS,
 
-    BLOCK_HAVE_DATA          =    8, //! full block available in blk*.dat
-    BLOCK_HAVE_UNDO          =   16, //! undo data available in rev*.dat
+    BLOCK_HAVE_DATA          =    8, //!< full block available in blk*.dat
+    BLOCK_HAVE_UNDO          =   16, //!< undo data available in rev*.dat
     BLOCK_HAVE_MASK          =   BLOCK_HAVE_DATA | BLOCK_HAVE_UNDO,
 
-    BLOCK_FAILED_VALID       =   32, //! stage after last reached validness failed
-    BLOCK_FAILED_CHILD       =   64, //! descends from failed block
+    BLOCK_FAILED_VALID       =   32, //!< stage after last reached validness failed
+    BLOCK_FAILED_CHILD       =   64, //!< descends from failed block
     BLOCK_FAILED_MASK        =   BLOCK_FAILED_VALID | BLOCK_FAILED_CHILD,
-	BLOCK_PROOF_OF_STAKE     =   128, //! is proof-of-stake block
-	BLOCK_STAKE_ENTROPY		 =	 256,
-	BLOCK_STAKE_MODIFIER	 =	 512,
+    BLOCK_PROOF_OF_STAKE     =   128, //! is proof-of-stake block
+    BLOCK_STAKE_ENTROPY      =   256,
+    BLOCK_STAKE_MODIFIER     =   512,
 
 };
 
