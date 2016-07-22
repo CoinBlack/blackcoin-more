fbc3049bd31657c93d8d6f6d4fe7d2d14380d0bb
Pieter Wuille <pieter.wuille@gmail.com>
2016-07-22 16:01:12 +0200

    Add extra message to avoid a long 'Loading banlist'

--- src/net.cpp
+++ src/net.cpp
@@ -2043,6 +2043,9 @@ void StartNode(boost::thread_group& threadGroup, CScheduler& scheduler)
 
     LogPrintf("Loaded %i addresses from peers.dat  %dms\n",
            addrman.size(), GetTimeMillis() - nStart);
+
+    uiInterface.InitMessage(_("Starting network threads..."));
+
     fAddressesInitialized = true;
 
     if (semOutbound == NULL) {
