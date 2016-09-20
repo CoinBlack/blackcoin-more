db57bbc0380bfe6e34e193303d77ede441a11db4
Jack Grigg <jack@z.cash>
2016-09-20 18:11:39 +1200

    Upgrade Boost to 1.62.0

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,8 +1,8 @@
 package=boost
-$(package)_version=1_59_0
-$(package)_download_path=http://sourceforge.net/projects/boost/files/boost/1.59.0
+$(package)_version=1_62_0
+$(package)_download_path=http://sourceforge.net/projects/boost/files/boost/1.62.0
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
-$(package)_sha256_hash=727a932322d94287b62abb1bd2d41723eec4356a7728909e38adb65ca25241ca
+$(package)_sha256_hash=36c96b0f6155c98404091d8ceb48319a28279ca0333fba1ad8611eb90afb2ca0
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
