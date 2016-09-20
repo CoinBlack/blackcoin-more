22bebdaacdc7f54c04670152973194a43e05cddd
Jonas Schnelli <dev@jonasschnelli.ch>
2016-09-20 16:18:24 +0200

    [Wallet] remove "unused" ThreadFlushWalletDB from removeprunedfunds

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -338,8 +338,6 @@ UniValue removeprunedfunds(const UniValue& params, bool fHelp)
         throw JSONRPCError(RPC_INTERNAL_ERROR, "Transaction does not exist in wallet.");
     }
 
-    ThreadFlushWalletDB(pwalletMain->strWalletFile);
-
     return NullUniValue;
 }
 
