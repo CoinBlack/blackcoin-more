39e1d793ce2f33f0d1dfe777b5cc3f64b67a35ae
Matt Corallo <git@bluematt.me>
2016-11-29 17:49:08 -0800

    Fix calculation of number of bound sockets to use
    Github-Pull: #9253
    Rebased-From: 9e1f46821d5bb69e2cbf25738eefa7c6cb99c838

--- src/init.cpp
+++ src/init.cpp
@@ -865,7 +865,9 @@ bool AppInit2(Config& config, boost::thread_group& threadGroup, CScheduler& sche
     }
 
     // Make sure enough file descriptors are available
-    int nBind = std::max((int)mapArgs.count("-bind") + (int)mapArgs.count("-whitebind"), 1);
+    int nBind = std::max(
+                (mapMultiArgs.count("-bind") ? mapMultiArgs.at("-bind").size() : 0) +
+                (mapMultiArgs.count("-whitebind") ? mapMultiArgs.at("-whitebind").size() : 0), size_t(1));
     int nUserMaxConnections = GetArg("-maxconnections", DEFAULT_MAX_PEER_CONNECTIONS);
     nMaxConnections = std::max(nUserMaxConnections, 0);
 
