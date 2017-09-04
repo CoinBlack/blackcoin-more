266da77e16cda0168f9df5d98fff41b006689455
janko33bd <janko33bd@gmail.com>
2017-09-04 22:39:25 +0100

    The fee is to low for the reference client.

--- src/amount.cpp
+++ src/amount.cpp
@@ -19,7 +19,7 @@ CFeeRate::CFeeRate(const CAmount& nFeePaid, size_t nSize)
 
 CAmount CFeeRate::GetFee(size_t nSize) const
 {
-    CAmount nFee = nSatoshisPerK*nSize / 1000;
+    CAmount nFee = nSatoshisPerK*(1 + nSize / 1000);
 
     if (nFee == 0 && nSatoshisPerK > 0)
         nFee = nSatoshisPerK;
