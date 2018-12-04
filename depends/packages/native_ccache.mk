78d2394670cb18632a47743cdc2c12a5a7af7308
Michel van Kessel <mvkessel@hotmail.com>
2018-12-04 17:05:33 +0100

    [depends] update native-ccache to 3.3.6
    from BCHABC : https://github.com/Bitcoin-ABC/bitcoin-abc/commit/0654a539663738f79b8478ad997b8afb5ef11948

--- depends/packages/native_ccache.mk
+++ depends/packages/native_ccache.mk
@@ -1,8 +1,8 @@
 package=native_ccache
-$(package)_version=3.3.4
+$(package)_version=3.3.6
 $(package)_download_path=https://samba.org/ftp/ccache
 $(package)_file_name=ccache-$($(package)_version).tar.bz2
-$(package)_sha256_hash=fa9d7f38367431bc86b19ad107d709ca7ecf1574fdacca01698bdf0a47cd8567
+$(package)_sha256_hash=eac8d2a5055014bebae1434d9b7c66c25d64323800808c27a4534cee167e6bea
 
 define $(package)_set_vars
 $(package)_config_opts=
