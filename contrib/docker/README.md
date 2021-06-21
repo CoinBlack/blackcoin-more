f72cb2d41d8de8c268cc0ab28b4c0ef1b14ad293
danielclough <danielclough@gmail.com>
2021-06-20 21:02:29 -0700

    option with minimal as default

--- contrib/docker/README.md
+++ contrib/docker/README.md
@@ -12,26 +12,27 @@
 
 Replace `danielclough` with your Dockerhub account name to use your own.
 
-### Full OS
-Blackcoin-More-Ubuntu (hundreds of MB)
+
+### Do you need a *complete OS* or just the *artifacts*?
+#### Full OS
+Blackcoin-More-Ubuntu (over a GB)
 ```
 docker pull danielclough/blackcoin-more-ubuntu-<ARCH>:<VERSION>
 docker run -itd  -v /home/$USER/.blackmore:/.blackmore --network=host --name=blackmore danielclough/blackcoin-more-ubuntu-<ARCH>:<VERSION> bash
 docker exec -itd blackmore blackmored -daemon
 docker exec -it blackmore blackmore-cli help
 ```
-### QT
 #### Install x11docker ([link]](https://github.com/mviereck/x11docker#shortest-way-for-first-installation))
+### QT
+	x11docker uses three options ([read more](https://github.com/mviereck/x11docker)):
+		`--desktop` indicates the image contains a complete OS 
+		`--clipboard` to allow copy/paste into and out of the container and host 
+		`--home` creates a default directory in `~/.local/share/x11docker/<IMAGENAME>`, **OR** specify a directory instead.
 
-x11docker uses three options ([read more](https://github.com/mviereck/x11docker)):
-	`--desktop` indicates the image contains a desktop OS 
-	`--clipboard` to allow copy/paste into and out of the container and host 
-	`--home` creates a default directory in `~/.local/share/x11docker/IMAGENAME`, but we specify a directory instead.
+	`sudo x11docker --desktop --clipboard --home=/home/$USER/x11blackmore danielclough/blackcoin-more-ubuntu-<ARCH>:v2.13.2.8 blackmore-qt`
+#### Minimal 
+Blackcoin-More-Minimal (hundreds of MB)
 
-`sudo x11docker --desktop --clipboard --home=/home/$USER/x11blackmore danielclough/blackcoin-more-ubuntu-<ARCH>:v2.13.2.8 blackmore-qt`
-
-### Minimal 
-Blackcoin-More-Minimal (tens of MB)
 ```
 docker pull danielclough/blackcoin-more-minimal-<ARCH>:<VERSION>
 docker run -itd  -v /home/$USER/.blackmore:/.blackmore --network=host --name=blackmore danielclough/blackcoin-more-minimal-<ARCH>:<VERSION> blackmored
--- contrib/docker/build.sh
+++ contrib/docker/build.sh
@@ -6,17 +6,17 @@ SYSTYPE=`lscpu | head -1 | tr -s ' ' | cut -d ' ' -f2`
 
 # DockerHub Account
 
-defaultDockerHub=danielclough
-read -p "What is your DockerHub Account Name? (default ${defaultDockerHub}): " DockerHub
+defaultDockerHub=blackcoinnl
+read -p "What is your DockerHub Account Name? (default: ${defaultDockerHub}): " DockerHub
 DockerHub=${DockerHub:-${defaultDockerHub}}
 if [ ${DockerHub} != ${defaultDockerHub} ]; then
-	sed -i "s/defaultDockerHub=danielclough/defaultDockerHub=${DockerHub}/" $0
+	sed -i "s/defaultDockerHub=blackcoinnl/defaultDockerHub=${DockerHub}/" $0
 fi
 
 # Git Account
 
 defaultHubLab=github
-read -p "Github or Gitlab? (default ${defaultHubLab}): " HubLab
+read -p "Github or Gitlab? (default: ${defaultHubLab}): " HubLab
 HubLab=${HubLab:-${defaultHubLab}}
 if [ ${HubLab} != ${defaultHubLab} ]; then
 	sed -i "s|defaultHubLab=github|defaultGit=${HubLab}|" $0
@@ -24,7 +24,7 @@ if [ ${HubLab} != ${defaultHubLab} ]; then
 fi
 
 defaultGit=CoinBlack
-read -p "Git account to use? (default ${defaultGit}): " Git
+read -p "Git account to use? (default: ${defaultGit}): " Git
 Git=${Git:-${defaultGit}}
 if [ ${Git} != ${defaultGit} ]; then
 	sed -i "s|defaultGit=CoinBlack|defaultGit=${Git}|" $0
@@ -34,7 +34,7 @@ fi
 # Git Branch
 
 defaultBranch=v2.13.2.8
-read -p "What branch/version? (default ${defaultBranch}): " BRANCH
+read -p "What branch/version? (default: ${defaultBranch}): " BRANCH
 BRANCH=${BRANCH:-${defaultBranch}}
 if [ ${BRANCH} != ${defaultBranch} ]; then
 	sed -i "s|defaultBranch=v2.13.2.8|defaultBranch=${BRANCH}|" $0
@@ -42,17 +42,25 @@ if [ ${BRANCH} != ${defaultBranch} ]; then
 	sed -i "s|ENV BRANCH=v2.13.2.8|ENV BRANCH=${BRANCH}|" ${BASE_DIR}/Dockerfile.ubuntu
 fi
 
+# x11 Desktop QT?
+defaultX11=no
+read -p "Are you going to need X11docker for QT (visual) client? (default: ${defaultX11}): " X11
+X11=${X11:-${defaultX11}}
+if [ ${X11} != ${defaultX11} ]; then
+	sed -i "s/defaultX11=no/defaultX11=${X11}/" $0
+fi
+
 # change branch for multi-stage build
-defaultUbase=danielclough/blackcoin-more-ubase-aarch64:v2.13.2.8
+defaultUbase=blackcoinnl/blackcoin-more-ubase-aarch64:v2.13.2.8
 ubase="${DockerHub}/blackcoin-more-ubase-${SYSTYPE}:${BRANCH}"
 if [ ${defaultUbase} != ${ubase} ]; then
 	sed -i "s|FROM danielclough/blackcoin-more-ubase-aarch64:${BRANCH} as build|FROM ${ubase} as build|" ${BASE_DIR}/Dockerfile.ubuntu
-	sed -i "s|defaultUbase=danielclough/blackcoin-more-ubase-aarch64:v2.13.2.8|defaultUbase=${ubase}|" $0
+	sed -i "s|defaultUbase=blackcoinnl/blackcoin-more-ubase-aarch64:v2.13.2.8|defaultUbase=${ubase}|" $0
 fi
 
 # timezone
 defaultTimezone=America/Los_Angeles
-read -p "What is your timezone? (default ${defaultTimezone}): " timezone
+read -p "What is your timezone? (default: ${defaultTimezone}): " timezone
 timezone=${timezone:-${defaultTimezone}}
 if [ ${timezone} != ${defaultTimezone} ]; then
 	sed -i "s|defaultTimezone=America/Los_Angeles|defaultTimezone=${timezone}|" $0
@@ -73,21 +81,21 @@ minimal="${DockerHub}/blackcoin-more-minimal-${SYSTYPE}:${BRANCH}"
 ubuntu="${DockerHub}/blackcoin-more-ubuntu-${SYSTYPE}:${BRANCH}"
 
 # build
-# ubase
+# ubase (base using ubuntu)
 docker build -t ${ubase} - --network=host < ${BASE_DIR}/Dockerfile.ubase
 docker run -itd  --network=host --name ubase ${ubase} bash
+# ubuntu (package with full ubuntu distro)
+if [ ${X11} != ${defaultX11} ]; then
+	docker build -t ${ubuntu} - --network=host < ${BASE_DIR}/Dockerfile.ubuntu
+	docker image push ${ubuntu}
+fi
 # minimal (only package binaries and scripts)
 docker cp ubase:/parts ${moreBuilder}
 cd ${moreBuilder}
 tar -C parts -c . | docker import - ${minimal}
-# ubuntu (package with full ubuntu distro)
-docker build -t ${ubuntu} - --network=host < ${BASE_DIR}/Dockerfile.ubuntu
+docker image push ${minimal}
 
 
 # cleanup
 docker container rm -f ubase
 docker image rm -f ${ubase}
-
-# push to docker hub
-docker image push ${minimal}
-docker image push ${ubuntu}
