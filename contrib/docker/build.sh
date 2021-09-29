9d09fd50ab8d1a7c909c7296cdab5188b13727e8
danielclough <danielclough@gmail.com>
2021-09-28 22:06:07 -0700

    conditional for minimal, branch, and hublab

--- contrib/docker/build.sh
+++ contrib/docker/build.sh
@@ -2,7 +2,15 @@
 
 BASE_DIR=$(dirname $(realpath $0 ))
 moreBuilder=${BASE_DIR}/moreBuilder
+
+defaultSysType=x86_64
 SYSTYPE=`lscpu | head -1 | tr -s ' ' | cut -d ' ' -f2`
+if [ ${SYSTYPE} != ${defaultSysType} ]; then
+	sed -i "s/defaultSysType=x86_64/defaultSysType=${SYSTYPE}/" $0
+	sed -i "s|x86_64|${SYSTYPE}|" ${BASE_DIR}/Dockerfile.ubuntu
+	sed -i "s|x86_64|${SYSTYPE}|" $0
+
+fi
 
 # DockerHub Account
 
@@ -11,51 +19,55 @@ read -p "What is your DockerHub Account Name? (default: ${defaultDockerHub}): " 
 DockerHub=${DockerHub:-${defaultDockerHub}}
 if [ ${DockerHub} != ${defaultDockerHub} ]; then
 	sed -i "s/defaultDockerHub=blackcoinnl/defaultDockerHub=${DockerHub}/" $0
+	sed -i "s|blackcoinnl|${DockerHub}|" ${BASE_DIR}/Dockerfile.ubuntu
+	sed -i "s|blackcoinnl|${DockerHub}|" $0
+
 fi
 
 # Git Account
 
-defaultHubLab=github
+defaultHubLab=gitlab
 read -p "Github or Gitlab? (default: ${defaultHubLab}): " HubLab
 HubLab=${HubLab:-${defaultHubLab}}
 if [ ${HubLab} != ${defaultHubLab} ]; then
-	sed -i "s|defaultHubLab=github|defaultGit=${HubLab}|" $0
-	sed -i "s|github|${HubLab}|" ${BASE_DIR}/Dockerfile.ubase
+	sed -i "s|defaultHubLab=gitlab|defaultHubLab=${HubLab}|" $0
+	sed -i "s|HUBLAB=gitlab|HUBLAB=${HubLab}|" ${BASE_DIR}/Dockerfile.ubase
+	sed -i "s|HUBLAB=gitlab|HUBLAB=${HubLab}|" ${BASE_DIR}/Dockerfile.minbase
+	sed -i "s|HUBLAB=gitlab|HUBLAB=${HubLab}|" $0
+
+
 fi
 
-defaultGit=CoinBlack
-read -p "Git account to use? (default: ${defaultGit}): " Git
-Git=${Git:-${defaultGit}}
-if [ ${Git} != ${defaultGit} ]; then
-	sed -i "s|defaultGit=CoinBlack|defaultGit=${Git}|" $0
-	sed -i "s|CoinBlack|${Git}|" ${BASE_DIR}/Dockerfile.ubase
+defaultGitName=blackcoin
+read -p "Git account to use? (default: ${defaultGitName}): " Git
+Git=${Git:-${defaultGitName}}
+if [ ${Git} != ${defaultGitName} ]; then
+	sed -i "s|defaultGitName=blackcoin|defaultGitName=${Git}|" $0
+	sed -i "s|GITNAME=blackcoin|GITNAME=${Git}|" ${BASE_DIR}/Dockerfile.ubase
+	sed -i "s|GITNAME=blackcoin|GITNAME=${Git}|" ${BASE_DIR}/Dockerfile.minbase
+	sed -i "s|GITNAME=blackcoin|GITNAME=${Git}|" $0
 fi
 
 # Git Branch
 
-defaultBranch=v2.13.2.8
+defaultBranch=master
 read -p "What branch/version? (default: ${defaultBranch}): " BRANCH
 BRANCH=${BRANCH:-${defaultBranch}}
 if [ ${BRANCH} != ${defaultBranch} ]; then
-	sed -i "s|defaultBranch=v2.13.2.8|defaultBranch=${BRANCH}|" $0
-	sed -i "s|ENV BRANCH=v2.13.2.8|ENV BRANCH=${BRANCH}|" ${BASE_DIR}/Dockerfile.ubase
-	sed -i "s|ENV BRANCH=v2.13.2.8|ENV BRANCH=${BRANCH}|" ${BASE_DIR}/Dockerfile.ubuntu
+	sed -i "s|defaultBranch=master|defaultBranch=${BRANCH}|" $0
+	sed -i "s|BRANCH=master|BRANCH=${BRANCH}|" ${BASE_DIR}/Dockerfile.ubase
+	sed -i "s|BRANCH=master|BRANCH=${BRANCH}|" ${BASE_DIR}/Dockerfile.minbase
+	sed -i "s|master|${BRANCH}|" ${BASE_DIR}/Dockerfile.ubuntu
+	sed -i "s|BRANCH=master|BRANCH=${BRANCH}|" $0
+	sed -i "s|master|${BRANCH}|" $0
 fi
 
 # x11 Desktop QT?
-defaultX11=no
+defaultX11=y
 read -p "Are you going to need X11docker for QT (visual) client? (default: ${defaultX11}): " X11
 X11=${X11:-${defaultX11}}
 if [ ${X11} != ${defaultX11} ]; then
-	sed -i "s/defaultX11=no/defaultX11=${X11}/" $0
-fi
-
-# change branch for multi-stage build
-defaultUbase=blackcoinnl/blackcoin-more-ubase-x86_64:v2.13.2.8
-ubase="${DockerHub}/blackcoin-more-ubase-${SYSTYPE}:${BRANCH}"
-if [ ${defaultUbase} != ${ubase} ]; then
-	sed -i "s|FROM danielclough/blackcoin-more-ubase-aarch64:${BRANCH} as build|FROM ${ubase} as build|" ${BASE_DIR}/Dockerfile.ubuntu
-	sed -i "s|defaultUbase=blackcoinnl/blackcoin-more-ubase-x86_64:v2.13.2.8|defaultUbase=${ubase}|" $0
+	sed -i "s/defaultX11=y/defaultX11=${X11}/" $0
 fi
 
 # timezone
@@ -65,6 +77,7 @@ timezone=${timezone:-${defaultTimezone}}
 if [ ${timezone} != ${defaultTimezone} ]; then
 	sed -i "s|defaultTimezone=America/Los_Angeles|defaultTimezone=${timezone}|" $0
 	sed -i "s|defaultTimezone=America/Los_Angeles|${timezone}|" ${BASE_DIR}/Dockerfile.ubase
+	sed -i "s|defaultTimezone=America/Los_Angeles|${timezone}|" ${BASE_DIR}/Dockerfile.minbase
 	sed -i "s|defaultTimezone=America/Los_Angeles|${timezone}|" ${BASE_DIR}/Dockerfile.ubuntu
 fi
 
@@ -76,22 +89,23 @@ echo ${SYSTYPE}
 echo ${timezone}
 
 # tag names
-ubase="${DockerHub}/blackcoin-more-ubase-${SYSTYPE}:${BRANCH}"
+base="${DockerHub}/blackcoin-more-base-${SYSTYPE}:${BRANCH}"
 minimal="${DockerHub}/blackcoin-more-minimal-${SYSTYPE}:${BRANCH}"
 ubuntu="${DockerHub}/blackcoin-more-ubuntu-${SYSTYPE}:${BRANCH}"
 
 # build
 # ubase (base using ubuntu)
-docker build -t ${ubase} - --network=host < ${BASE_DIR}/Dockerfile.ubase
 # ubuntu (package with full ubuntu distro)
-if [ ${X11} != ${defaultX11} ]; then
+if ! [[ ${X11} =~ [no|n] ]]; then
+	docker build -t ${base} - --network=host < ${BASE_DIR}/Dockerfile.ubase
 	docker build -t ${ubuntu} - --network=host < ${BASE_DIR}/Dockerfile.ubuntu
 	docker image push ${ubuntu}
+else
+	docker build -t ${base} - --network=host < ${BASE_DIR}/Dockerfile.minbase
 fi
 # minimal (only package binaries and scripts)
-docker run -itd  --network=host --name ubase ${ubase} bash
-docker cp ubase:/parts ${moreBuilder}
+docker run -itd  --network=host --name base ${base} bash
+docker cp base:/parts ${moreBuilder}
 cd ${moreBuilder}
 tar -C parts -c . | docker import - ${minimal}
 docker image push ${minimal}
-docker container stop ubase
