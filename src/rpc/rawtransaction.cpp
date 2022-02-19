98fecd1c051c0155ca8cd10db927dcb3c30c1bd1
lateminer <9951982+lateminer@users.noreply.github.com>
2022-02-20 00:09:27 +0200

    Show nTime field in transaction JSON output

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -68,6 +68,7 @@ void TxToJSON(const CTransaction& tx, const uint256 hashBlock, UniValue& entry)
     entry.push_back(Pair("txid", tx.GetHash().GetHex()));
     entry.push_back(Pair("size", (int)::GetSerializeSize(tx, SER_NETWORK, PROTOCOL_VERSION)));
     entry.push_back(Pair("version", tx.nVersion));
+    entry.push_back(Pair("time", (int64_t)tx.nTime));
     entry.push_back(Pair("locktime", (int64_t)tx.nLockTime));
 
     UniValue vin(UniValue::VARR);
