79469051a1a3e47d6420c8d83b2c7e330902a9d7
lateminer <9951982+lateminer@users.noreply.github.com>
2022-04-28 20:47:28 +0300

    Don't override transaction time
    https://github.com/peercoin/peercoin/commit/a33ca81f4348214e561e79d4141701f5f9ff364d

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -110,7 +110,6 @@ void TxToJSON(const CTransaction& tx, const uint256 hashBlock, UniValue& entry)
             CBlockIndex* pindex = (*mi).second;
             if (chainActive.Contains(pindex)) {
                 entry.push_back(Pair("confirmations", 1 + chainActive.Height() - pindex->nHeight));
-                entry.push_back(Pair("time", pindex->GetBlockTime()));
                 entry.push_back(Pair("blocktime", pindex->GetBlockTime()));
             }
             else
