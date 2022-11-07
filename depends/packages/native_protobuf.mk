df2cedc1c9a861ab9f6e8a98da5a2c3f5ed6b859
Crypto Tafkar <cryptotafkar@protonmail.com>
2022-11-07 18:30:23 +0100

    protobuf dependencies - update/cleanup

--- depends/packages/native_protobuf.mk
+++ depends/packages/native_protobuf.mk
@@ -1,8 +1,8 @@
 package=native_protobuf
-$(package)_version=3.17.3
+$(package)_version=3.20.2
 $(package)_download_path=https://github.com/google/protobuf/releases/download/v$($(package)_version)
 $(package)_file_name=protobuf-cpp-$($(package)_version).tar.gz
-$(package)_sha256_hash=51cec99f108b83422b7af1170afd7aeb2dd77d2bcbb7b6bad1f92509e9ccf8cb
+$(package)_sha256_hash=a0167e2ba24bba0a180fbc9392f3a43e749d7a26e630fe9c1a1ba32a53675ac3
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared --without-zlib
