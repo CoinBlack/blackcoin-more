4992b595e04d7ad9f9b9d7511c6e8e8d9585c229
lateminer <9951982+lateminer@users.noreply.github.com>
2022-11-16 23:27:17 +0200

    Add checkpoint at block 4232630

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -163,9 +163,10 @@ public:
                     ( 38425, uint256S("0x62bf2e9701226d2f88d9fa99d650bd81f3faf2e56f305b7d71ccd1e7aa9c3075"))  // hardfork
                     (254348, uint256S("0x9bf8d9bd757d3ef23d5906d70567e5f0da93f1e0376588c8d421a95e2421838b"))  // minor network split
                     (319002, uint256S("0x0011494d03b2cdf1ecfc8b0818f1e0ef7ee1d9e9b3d1279c10d35456bc3899ef"))  // hardfork
-                    (872456, uint256S("0xe4fd321ced1de06213d2e246b150b4bfd8c4aa0989965dce88f2a58668c64860")), // hardfork
-                    1444028528, // * UNIX timestamp of last checkpoint block
-                    2774767,    // * total number of transactions between genesis and last checkpoint
+                    (872456, uint256S("0xe4fd321ced1de06213d2e246b150b4bfd8c4aa0989965dce88f2a58668c64860"))  // hardfork
+                    (4232630, uint256S("0xae0c2a9bd13746e2887ca57bf1046b3c787a5ed1068fd1633a3575f08ee291fc")),
+                    1668631296, // * UNIX timestamp of last checkpoint block
+                    13562393,    // * total number of transactions between genesis and last checkpoint
                                 //   (the tx=... number in the SetBestChain debug.log lines)
                     3500.0      // * estimated number of transactions per day after checkpoint
         };
