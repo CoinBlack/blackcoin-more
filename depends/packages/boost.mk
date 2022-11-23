5c38ace5c56e74bd206014c750a411a4e36177b1
BlackcoinDev <dev@blackcoin.dev>
2022-11-23 20:38:51 +0100

    final build system adjustments

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,7 +1,7 @@
 package=boost
-$(package)_version=1_77_0
-$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$(subst _,.,$($(package)_version))/source/
-$(package)_file_name=boost_$($(package)_version).tar.bz2
+$(package)_version=1.77.0
+$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$($(package)_version)/source/
+$(package)_file_name=boost_$(subst .,_,$($(package)_version)).tar.bz2
 $(package)_sha256_hash=fc9f85fc030e233142908241af7a846e60630aa7388de9a5fafb1f3a26840854
 $(package)_dependencies=native_b2
 
