4bc5d4dbfaa684978ff93800f296d01a52955255
lateminer <9951982+lateminer@users.noreply.github.com>
2024-04-29 22:58:17 +0200

    wallet: Always start fee calculation with MIN_TX_FEE

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2621,7 +2621,7 @@ bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CWalletTx& wt
             std::vector<COutput> vAvailableCoins;
             AvailableCoins(vAvailableCoins, true, coinControl);
 
-            nFeeRet = Params().GetConsensus().IsProtocolV3_1(txNew.nTime) ? MIN_TX_FEE : 0;
+            nFeeRet = MIN_TX_FEE;
             // Start with no fee and loop until there is enough fee
             while (true)
             {
