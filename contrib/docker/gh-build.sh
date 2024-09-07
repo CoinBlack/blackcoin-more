4faf0c1c375760b963edcf43a879164e3bdc061c
BlackcoinDev <dev@blackcoin.dev>
2024-09-07 16:28:38 +0200

    build: improve docker build

--- contrib/docker/gh-build.sh
+++ contrib/docker/gh-build.sh
@@ -9,8 +9,8 @@ export DockerHub=blackcoinnl
 export HUBLAB=github
 export GITNAME=CoinBlack
 export BRANCH=${GIT_CURRENT_BRANCH}
-sed -i "master|${BRANCH}|" ${BASE_DIR}/Dockerfile.ubase
-sed -i "master|${BRANCH}|" ${BASE_DIR}/Dockerfile.ubuntu
+sed -i "s/master/${BRANCH}/" ${BASE_DIR}/Dockerfile.ubase
+sed -i "s/master/${BRANCH}/" ${BASE_DIR}/Dockerfile.ubuntu
 export TZ=Etc/UTC
 
 echo "${GITHUB_ENV} = GITHUB_ENV"
@@ -28,12 +28,25 @@ ubuntu="${DockerHub}/blackcoin-more-ubuntu-${SYSTYPE}:${BRANCH}"
 # build
 # ubase (base using ubuntu)
 # ubuntu (package with full ubuntu distro)
-docker build -t ${base} - --network=host < ${BASE_DIR}/Dockerfile.ubase
-docker build -t ${ubuntu} - --network=host < ${BASE_DIR}/Dockerfile.ubuntu
+docker build -t ${base} --network=host -f ${BASE_DIR}/Dockerfile.ubase ${BASE_DIR}
+if [ $? -ne 0 ]; then
+    echo "Error building base image" && exit 1
+fi
+docker build -t ${ubuntu} --network=host -f ${BASE_DIR}/Dockerfile.ubuntu ${BASE_DIR}
+if [ $? -ne 0 ]; then
+    echo "Error building ubuntu image" && exit 1
+fi
 docker image push ${ubuntu}
+if [ $? -ne 0 ]; then
+    echo "Error pushing image" && exit 1
+fi
+
 
 # minimal (only package binaries and scripts)
 docker run -itd  --network=host --name base ${base} bash
 docker cp base:/parts ${moreBuilder}
 cd ${moreBuilder}
 tar -c . | docker import - ${minimal} &&  docker image push ${minimal}
+if [ $? -ne 0 ]; then
+    echo "Error creating and pushing minimal image" && exit 1
+fi
