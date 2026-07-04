219d07890f90ea7142168b5f2c8d9f7b4e4cd9ed
BlackcoinDev <dev@blackcoin.dev>
2026-07-05 01:01:17 +0200

    consensus: add taproot bip-9 start
    Sunday, July 12, 2026 at 5:00:00 AM CET

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -125,7 +125,7 @@ public:
 
         // Deployment of Taproot (BIPs 340-342)
         consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].bit = 2;
-        consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].nStartTime = Consensus::BIP9Deployment::NEVER_ACTIVE;
+        consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].nStartTime = 1783832400; // Sunday, July 12, 2026 at 5:00:00 AM CET
         consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
         consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].min_activation_height = 0; // No activation delay
 
